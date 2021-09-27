8e59f2f2a50114ece8ca6f30d672cc4d1920bf22
alejandro.gonzalez04 <alejandro.gonzalez04@globant.com>
2021-09-27 20:37:55 -0300

    agregando components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes/Routes';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
+   <React.StrictMode>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
