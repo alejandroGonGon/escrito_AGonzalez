b93a405df66f4f7e0bd1003af1fef1db8d93e9ae
alejandro.gonzalez04 <alejandro.gonzalez04@globant.com>
2021-09-27 22:05:23 -0300

    agregando archivos faltantes

--- src/pages/Meetings.jsx
+++ src/pages/Meetings.jsx
@@ -1,26 +1,50 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
+const axiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${process.env.REACT_APP_TOKEN}`,
+  },
+  params: {},
+});
+
 const Meetings = () => {
-    const [meeting, setMeeting] = useState('');
+    const [topic, setTopic] = useState('');
+    const [duration, setDuration] = useState('');
+    const [date, setDate] = useState(new Date());
+    
     const [listMeetings, setListMeetings] = useState([]);
     const [error, setError] = useState('');
   
     useEffect(() => {
         const getMeetings = async () => {
           try {
-            const response = await axios.get('');
+            const response = await axiosInstance.get('/users/me/meetings')
             setListMeetings(response.data);
         } catch (err) {
             setError('There was an error bringing the Meetings');
-            console.log(err)
+            console.log(error)
           }
         };
         getMeetings()
       },[]);
 
-    const handleSubmit = () => {
-        // setMeeting({});
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      try {
+        const response = await axiosInstance.post('/users/me/meetings',{
+          topic: topic,
+          duration: duration,
+          date: date
+        });
+        console.log(response + 'si');
+    } catch (err) {
+        setError('There was an error bringing the Meetings');
+        console.log(err)
+        console.log('no');
+      }
     };
     const renderMeetings = () => {
         // 
@@ -28,15 +52,16 @@ const Meetings = () => {
     return (
     <div>
         <h1>Create Meetting</h1>
-        <input type="text" id='topic' placeholder='topic'/>
-        <input type="datetime-local" id='date' placeholder='date'/>
-        <input type="text" id='duration' placeholder='duration'/>
-        <input type="button" nSubmit={handleSubmit} value="create" />
-    <p>Waiting to send</p>
-
+        <form onSubmit={handleSubmit} >
+        <input type="text" id='topic' placeholder='topic' onChange={e => setTopic(e.target.value)}/>
+        <input type="datetime-local" id='date' placeholder='date'  onChange={e => setDate(e.target.value)}/>
+        <input type="text" id='duration' placeholder='duration'  onChange={e => setDuration(e.target.value)}/>
+        <input type="submit" value="create" />
+        </form>
+        
     <h1>See Meettings</h1>
         <div>
-        {renderMeetings}
+        {renderMeetings()}
         </div>
   </div>
     );
